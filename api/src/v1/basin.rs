88976964f7e11a995206258bf396326d11f9bf67
Stephen Balogh <stephen@s2.dev>
2026-05-18 16:41:43 -0700

    default

--- api/src/v1/basin.rs
+++ api/src/v1/basin.rs
@@ -138,7 +138,7 @@ pub struct EnsureBasinRequest {
     /// Basin configuration.
     pub config: Option<BasinConfig>,
     /// Basin scope.
-    /// If omitted when creating, defaults to `aws:us-east-1`.
+    /// If omitted when creating, uses the configured default scope.
     /// This cannot be changed.
     pub scope: Option<String>,
 }
@@ -154,6 +154,6 @@ pub struct CreateBasinRequest {
     /// Basin configuration.
     pub config: Option<BasinConfig>,
     /// Basin scope.
-    /// If omitted, defaults to `aws:us-east-1`.
+    /// If omitted when creating, uses the configured default scope.
     pub scope: Option<String>,
 }
